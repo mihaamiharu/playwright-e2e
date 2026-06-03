9ebf7f85ac7e17588b485726068b3328a62cb99e
mihaamiharu <ekkisyam23@gmail.com>
2026-06-03 22:10:23 +0700

    test: increase global test timeout to 30s

--- playwright.bdd.config.ts
+++ playwright.bdd.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? 2 : undefined,
-  timeout: 20_000,
+  timeout: 30_000,
   expect: {
     timeout: 10_000,
     toHaveScreenshot: {
